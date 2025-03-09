372054dd9a86e21be7dcbbe4d0a66f2f7f56c915
Adérito Muffins <157916148+Aderito-Muffins@users.noreply.github.com>
2025-03-09 21:48:37 +0200

    Update authRouter.ts

--- server/src/auth/authRouter.ts
+++ server/src/auth/authRouter.ts
@@ -2,7 +2,8 @@ import express from "express";
 import { compare } from "bcrypt-ts";
 import jwt from "jsonwebtoken"; // Para criação de JWT
 import { getUser } from "../lib/db/queries"; // Para consultar o usuário na base de dados
-
+import ms from "ms";
+import { AnyColumn } from "drizzle-orm";
 const authRouter = express.Router();
 
 // Função para gerar token JWT
@@ -15,8 +16,14 @@ const generateToken = (user: any) => {
     throw new Error("JWT_SECRET is not defined");
   }
 
+  // Verifique se JWT_EXPIRES é definido e é um número válido
+  const jwtExpires = process.env.JWT_EXPIRES;
+  if (!jwtExpires) {
+    throw new Error("jwtExpires is not defined");
+  }
+
   const token = jwt.sign({ id: user.id, email: user.email }, jwtSecret, {
-    expiresIn: process.env.JWT_EXPIRES || "1h", // Default é 1 hora se não especificado no .env
+    expiresIn: Number(jwtExpires), // Certificando-se de que o valor é numérico
   });
 
   return token;
@@ -57,7 +64,7 @@ authRouter.post("/login", async (req, res) => {
       message: "Login bem-sucedido",
       data: { token, user },
     });
-  } catch (error) {
+  } catch (error: any) {
     return res
       .status(500)
       .json({ code: 0, message: "Erro no servidor", data: error.message });
@@ -96,7 +103,7 @@ authRouter.post("/check", (req, res) => {
       message: "Token válido",
       data: { isValid: true, decoded }, // Inclui `isValid` e os dados decodificados
     });
-  } catch (error) {
+  } catch (error: any) {
     return res.status(401).json({
       code: 0,
       message: "Token inválido ou expirado",
